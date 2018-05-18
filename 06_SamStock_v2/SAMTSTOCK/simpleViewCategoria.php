95c471f475745d88eb5ec48358b73edc04bd3e62
LucaRausa <31542209+LucaRausa@users.noreply.github.com>
2018-05-18 09:05:11 +0200

    simpleview

--- 06_SamStock_v2/SAMTSTOCK/simpleViewCategoria.php
+++ 06_SamStock_v2/SAMTSTOCK/simpleViewCategoria.php
@@ -1,31 +1,89 @@
+<?php 
+$categoria = $_GET['categoria'];
+?>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- stili -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/index.css" rel="stylesheet">
         <link rel="icon" type="image/png" href="img/favicon.png"> 
+        <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/0.9.0rc1/jspdf.min.js"></script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <title>Prodotti di <?php echo $categoria; ?></title>
     </head>
-    <style>
-        td {
-            padding: 30px;
+    <style>        
+        img {
+            max-width: 100%;
+            min-width: 90px;
+            width: 200px;
+            height: auto;
+            margin: auto;
         }
         
-        tr:nth-child(odd)
-        {
-            background-color: #F9F9F9;
+        h3 {
+            font-size: 35px;
+            text-align: center;
+            margin: auto;
         }
         
-        th {
-            border: #d3d3d3 1px solid;
-            background-color: #F0FFFF; 
+        h1 {
+            margin: 10px; 
+            text-align: center;
+            background: #f2f2f2;
+            border: gray 1px solid;
+            min-height: 43px;
+        }
+        
+        p {
+            text-align: center;
+        }
+        
+        .row:nth-of-type(odd) {
+            background: #edfeff;
+        }
+        .row:nth-of-type(even) {
+           background: #FFFFFF;
+        }
+        
+        .row {
+            text-align: center;
+            border: 1px gray solid;
+            margin-left: 10px;
+            margin-right: 10px;
+            height: auto;
+        }
+        
+        .cell {
+            display: table-cell;
+            text-align: center;
+            display: flex;
+            height: 35%;
+            align-items: center;
+            margin: auto;
+        }
+        
+        .footer {
+            margin-top: auto;
+            height: auto;
+        }
+        button {
+            float: left;
+            max-height: 40px;
+            vertical-align: middle;
+        }
+        
+        i {
+            height: auto;
+            width: auto;
+            vertical-align: middle;
         }
     </style>
-    <body>
+    <body id="bodyId">
         <?php
         include 'mysqlcon.php';
         include 'barcodeReader/barcode.php';
-
-        $categoria = $_GET['categoria'];
        
         $id_cat = mysqli_query($con, "SELECT ID_Categoria FROM categorie WHERE NomeC LIKE '" . $categoria . "';");
 
@@ -33,51 +91,76 @@
         $symbolUPC = 'ean-13';
         $symbolQR = 'qr';
         $counter = 0;
-        echo "<table style='text-align: center; margin: auto; width: 96%; margin-left: 2%; margin-right: 2%px; border-collapse: collapse;'>";
-        echo "<th colspan=4><h1 style='margin: auto; text-align: center;'>Categoria: " . $categoria . "</h1></th>";
-        while ($row1 = mysqli_fetch_array($id_cat)) {
+//        echo "<div class='cont' id='cont'><table>";
+//        echo "<th colspan=4><button id='printButton' onclick='printPage()'><i class='glyphicon glyphicon-print'></i></button><h1 id='titolo'>Categoria: " . $categoria . "</h1></th>";
+//        while ($row1 = mysqli_fetch_array($id_cat)) {
+//            $result = mysqli_query($con, "SELECT * FROM prodotti WHERE Categoria LIKE '" . $row1["ID_Categoria"] . "';");
+//
+//            while ($row2 = mysqli_fetch_array($result)) {
+//                $data = $row2['Numero di serie'];
+//                $img = $row2["Icona"];
+//
+//                $imagePathUPC = 'img/barcodeGeneratedUPC' . $counter . '.png';
+//                $imagePathQR = 'img/barcode_GeneratedQR' . $counter . '.png';
+//
+//                $imageUPC = $barcodeGenerator->render_image($symbolUPC, $data, '');
+//                $imageQR = $barcodeGenerator->render_image($symbolQR, $data, '');
+//
+//                imagepng($imageUPC, $imagePathUPC);
+//                imagepng($imageQR, $imagePathQR);
+//                imagedestroy($imageUPC);
+//                imagedestroy($imageQR);             
+//                
+//                echo "<tr>";
+//                        echo "<td><h3>" . $row2['NomeP'] . "</h3></td>";
+//                        echo "<td><img src='$img' /></td>";
+//                        echo "<td><img src='$imagePathUPC' /></td>";
+//                        echo "<td><img src='$imagePathQR' /></td>";
+//                echo "</tr>";
+//                $counter++;
+//            }
+//        }
+//        echo "</table></div>";
+        
+        echo "<form method='post' action='printSimpleViewCategoria.php/?categoria=".$categoria."'><h1><button id='printButton'><i class='glyphicon glyphicon-print'></i></button>Prodotti di ".$categoria."</h1></form>";
+        
+        while ($row1 = mysqli_fetch_array($id_cat))
+        {
             $result = mysqli_query($con, "SELECT * FROM prodotti WHERE Categoria LIKE '" . $row1["ID_Categoria"] . "';");
-
-            while ($row2 = mysqli_fetch_array($result)) {
+        
+            while($row2 = mysqli_fetch_array($result)) 
+            {
                 $data = $row2['Numero di serie'];
-                $img = $row2["Icona"];
-
+                $link = "localhost:3600/aggiungiprodotto.php?idprod=".$row2["ID_Prodotto"];
+                $img = $row2['Icona'];
+                
                 $imagePathUPC = 'img/barcodeGeneratedUPC' . $counter . '.png';
                 $imagePathQR = 'img/barcode_GeneratedQR' . $counter . '.png';
-
+                
                 $imageUPC = $barcodeGenerator->render_image($symbolUPC, $data, '');
-                $imageQR = $barcodeGenerator->render_image($symbolQR, $data, '');
+                $imageQR = $barcodeGenerator->render_image($symbolQR, $link, '');
 
                 imagepng($imageUPC, $imagePathUPC);
                 imagepng($imageQR, $imagePathQR);
                 imagedestroy($imageUPC);
                 imagedestroy($imageQR);
-
-                echo "<tr style='margin: auto; border-bottom: 1px solid #d3d3d3;'>";
-                echo "<td style='width:25%;'><h3 style='font-size: 30px;'>" . $row2['NomeP'] . "</h3></td>";
-                echo "<td style='width:25%;'><img src='$imagePathUPC' width='200' height='150' /></td>";
-                echo "<td style='width:25%;'><img src='$imagePathQR' width='150' height='150' /></td>";
-                echo "<td style='width:25%;'><img src='$img'  width='150' height='150' /></td>";
-                echo "</tr>";
+                
+                echo '<div class="row">';
+                    echo '<div class="col-md-3 col-xs-6 divTableCell cell"><h3>'.$row2['NomeP'].'</h3></div>';
+                    echo '<div class="col-md-3 col-xs-6 divTableCell cell"><img src="'.$img.'" /></div>';
+                    echo '<div class="col-md-3 col-xs-6 divTableCell cell"><img src="'.$imagePathUPC.'" /></div>';
+                    echo '<div class="col-md-3 col-xs-6 divTableCell cell"><img src="'.$imagePathQR.'" /></div>';
+                echo '</div>';
                 $counter++;
             }
         }
-        echo "</table>";
         ?>
+        <div id="editor"></div>
         <!-- footer -->
         <div class="footer">
             <div class="container">
-                <p class="text-muted">Powered by Angelo Sanker Copyright &copy; 2014 SAMT.</p>
+                <p class="text-muted">Powered by Angelo Sanker Copyright &copy; 2014 & Luca Rausa - Elia Manassero &copy; 2018 SAMT.</p>
             </div>
         </div>
-        <script>
-            $(document).ready( function()
-            {
-                $("tr:odd").css({
-                    "background-color":"#000";
-                    "color":"#fff";
-                });
-            });
-        </script>
     </body>
 </html>
